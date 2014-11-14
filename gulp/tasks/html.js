e6c6a1e44b186e1378c08c61214e1f70f431b57e
Federico Brigante <bfred-it@users.noreply.github.com>
2014-11-14 13:02:25 +0100

    Only process changed jade files

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,37 +1,87 @@
 var gulp   = require('gulp');
 var config = require('../config').markup
 var log    = require('../util/logging');
+var es     = require('event-stream');
+
 
 var $ = require('gulp-load-plugins')();
 
 gulp.task('html', function() {
-	var phpTemplatesFilter = $.filter('**/*.html.php');
-	var jadeFilter = $.filter('**/*.jade');
-	var noPartialsFilter = $.filter(function (file) {
+	var phpTemplatesOnly = '**/*.html.php';
+	var jadeFilesOnly = '**/*.jade';
+	var htmlFilesOnly = '**/*.html';
+	var noPartials = function (file) {
 		return !/\/_/.test(file.path) || !/^_/.test(file.relative);
-	});
+	};
+
 	var stream = gulp.src(config.src)
-		.pipe($.plumber({errorHandler: log.onError}))
+		.pipe($.plumber({errorHandler: log.onError}));
 
-		.pipe(noPartialsFilter)//don't process partials
+	//separate html-only files
+	var htmlStream = stream.pipe($.filter(htmlFilesOnly))
 
-		//process php templates
-		.pipe(phpTemplatesFilter)
-		.pipe($.php2html())
-		.pipe($.rename(function (path) {
-			path.extname = ""
-		}))
-		.pipe(phpTemplatesFilter.restore())
+		//log what's being worked on
+		.pipe(log.working('<%= file.relative %>'));
 
-		.pipe($.changed(config.dest)) // Ignore unchanged files
+	//process jade
+	var jadeStream = stream.pipe($.filter(jadeFilesOnly))
+
+		//only pass unchanged *main* file and *all* the partials
+		.pipe($.changed(config.dest, {extension: '.html'}))
+
+		//filter out unchanged partials, but it only works when watching
+		.pipe($.if(global.isWatching, $.cached('html')))
+
+		//find files that depend on the files that have changed
+		.pipe($.jadeInheritance({basedir: 'app/htdocs'}))
+
+		//filter out partials
+		.pipe($.filter(noPartials))
+
+		//log what's being worked on
 		.pipe(log.working('<%= file.relative %>'))
 
-		//process jade
-		.pipe(jadeFilter)
-		.pipe($.jade())
-		.pipe(jadeFilter.restore())
+		//add "relativeRoot" variable in Jade templates
+		.pipe($.data(function (file) {
+			var template = {
+				relativePath: file.history[0].replace(file.base, '')
+			};
+			var depth = (template.relativePath.match(/\//g) || []).length;
+			var relativeRoot = new Array(depth + 1).join( '../' );
+			return {
+				// template: template,
+				relativeRoot: relativeRoot
+			};
+		}))
+
+		//process jade templates
+		.pipe($.jade());
 
 
+	//process php templates
+	//TODO: find depency tree like with jadeInheritance
+	var phpStream = stream.pipe($.filter(phpTemplatesOnly))
+
+		//filter out partials
+		.pipe($.filter(noPartials))
+
+		//log what's being worked on
+		.pipe(log.working('<%= file.relative %>'))
+
+		//parse php templates and output static html files
+		.pipe($.php2html())
+
+		//since php templates had a .html.php extension, now it's .html.html
+		//so remove the second ".html"
+		.pipe($.rename(function (file) {
+			file.extname = '';
+		}));
+
+
+	//process all the html files, both static and generated ones
+	stream = es.merge(jadeStream, phpStream, htmlStream)
+
+		//compress html files, including inline CSS and JS
 		.pipe($.compressor({
 			type: 'html',
 			// 'preserve-server-script': true,
@@ -40,13 +90,17 @@ gulp.task('html', function() {
 			// 'preserve-comments': true
 		}))
 
+		//save all the files
 		.pipe(gulp.dest(config.dest))
+
+		//log all the output files
 		.pipe(log.done('<%= file.relative %>'));
 
 
+	//validate html files
 	stream
-		.pipe($.filter('**/*.html'))
-		.pipe($.w3cjs());//should be done asynchronously, so livereload doesn't have to wait for it
+		.pipe($.filter(htmlFilesOnly))
+		.pipe($.w3cjs());//should be done asynchronously, so next tasks doesn't have to wait for it
 
 	return stream;
 });
