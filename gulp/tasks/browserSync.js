6d1f0aff4d2c4e9b9fcc7b2a9ba3c28cebe22e67
Federico <bfred-it@users.noreply.github.com>
2015-03-09 22:08:39 +0100

    gulp: Faster watch start

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -3,6 +3,6 @@ var browserSync = require('browser-sync');
 var gulp        = require('gulp');
 var config      = require('../config').browserSync;
 
-gulp.task('browserSync', ['build'], function() {
+gulp.task('browserSync', ['scripts'], function() {
   browserSync(config);
 });
