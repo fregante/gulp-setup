aabd1c79a5a8dcb77c3f6616ef11bd239e2032b9
Federico Brigante <bfred-it@users.noreply.github.com>
2015-07-13 16:38:18 -0700

    + postcss

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -3,39 +3,62 @@ var gulp         = require('gulp');
 var log          = require('../util/logging');
 var config       = require('../config').sass;
 var autoprefixer = require('autoprefixer-core');
-var postcssImport= require('postcss-import');
+var loadPlugins  = require('gulp-load-plugins');
+var plugin       = loadPlugins();
+var postcss      = loadPlugins({pattern: 'postcss-*', replaceString: /^postcss-/});
+var path         = require('path');
 
-var $ = require('gulp-load-plugins')();
-
-gulp.task('css', ['images'], function () {
+gulp.task('css', function () {
 	var noPartials = function (file) {
 		var isWin = /^win/.test(process.platform);
+		var cwd = process.cwd();
+		var relatvePath = path.relative(cwd,file.path);
 		if (isWin) {
-			return !/\\_/.test(file.path);
+			return !/\\_/.test(relatvePath);
 		}
-		return !/\/_/.test(file.path);
+		return !/\/_/.test(relatvePath);
 	};
 
 	var processors = [
+		// advanced
+		postcss.assets(),
+
+		// properties
+		postcss.position(),
+		// postcss.short(),
+		postcss.colorPalette(),
+		postcss.willChange(),
+		// postcss.propertyLookup(),
+		postcss.easings(),
+
+		// selectors
+		postcss.pseudoClassAnyLink(),
+		postcss.selectorNot(),
+
+		// queries
+		// postcss.mediaMinmax(),
+		postcss.import(),
+
 		autoprefixer({
 			browsers: ['last 2 versions', 'ie 9']
 		}),
-		postcssImport()
+
+		postcss.logWarnings(),
 	];
 
 	return gulp.src(config.src)
-		.pipe($.plumber({errorHandler: log.onError}))
-		.pipe($.filter(noPartials))
+		.pipe(plugin.plumber({errorHandler: log.onError}))
+		.pipe(plugin.filter(noPartials))
 		.pipe(log.working('<%= file.relative %>'))
 
-		.pipe($.sassBulkImport())
-		.pipe($.sass({
+		.pipe(plugin.sassBulkImport())
+		.pipe(plugin.sass({
 			outputStyle: 'expanded',
 			sourcemap: global.isWatching,
 			includePaths: config.includePaths
 		}))
-		.pipe($.postcss(processors))
-		.pipe($.if(!global.isWatching, $.csso(/*preventRestructuring*/true)))
+		.pipe(plugin.postcss(processors))
+		// .pipe(plugin.if(!global.isWatching, plugin.csso(/*preventRestructuring*/true)))
 
 		.pipe(gulp.dest(config.dest))
 		.pipe(log.done('<%= file.relative %>'));
