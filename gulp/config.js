87c3e3a0a1d5ef6edf774e6b62039ddd96404e79
Federico Brigante <bfred-it@users.noreply.github.com>
2015-07-13 16:29:52 -0700

    Improved browserify notification

--- gulp/config.js
+++ gulp/config.js
@@ -21,6 +21,27 @@ module.exports = {
 		// 	forms: true,
 		// 	scroll: false
 		// },
+		notify: {
+        styles: [
+					'display: none',
+					'padding: 15px',
+					'font-family: sans-serif',
+					'position: fixed',
+					'font-size: 0.9em',
+					'z-index: 9999999',
+					'right: 0px',
+					'left: 0px',
+					'bottom: 0',
+					'background-color: #1B2032',
+					'margin: 0',
+					'color: white',
+					'text-align: center',
+					'-webkit-pointer-events: none',
+					'-moz-pointer-events: none',
+					'-ms-pointer-events: none',
+					'pointer-events: none'
+    	]
+    },
 		logFileChanges: false,
 		open: false,
 		files: [
